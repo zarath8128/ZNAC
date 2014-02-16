cbff3f7eb71e44a1d107c507449ad6125fffa3a1
zarath <okamoto6496@gmail.com>
2014-02-16 18:14:03 +0900

    2014/2/16

--- basic/general.h
+++ basic/general.h
@@ -9,6 +9,18 @@ namespace ZNAC
 	public:
 		constexpr virtual T *Clone() = 0;
 	};
+
+	template<class T>
+	constexpr const T &MAX(const T &a, const T &b){return ((a < b)?(b):(a));}
+
+	template<class T>
+	constexpr const T &MIN(const T &a, const T &b){return ((a > b)?(b):(a));}
+
+	template<class T>
+	constexpr T ABS(const T &a){return ((a < 0)?(-a):(a));}
+
+	template<class T>
+	constexpr int SIGN(const T &a){return ((a < 0)?(-1):(1));}
 }
 
 #endif
