4b411f7c4f866b774de2796ad90876488dd81edd
Mamoru OKAMOTO <okamoto@apple07.(none)>
2014-04-18 19:56:07 +0900

    test code update

--- Visualize/GLFW/test.cpp
+++ Visualize/GLFW/test.cpp
@@ -2,6 +2,8 @@
 #include <GL/glext.h>
 #include <iostream>
 #include "Window.h"
+#include "Capture.h"
+#include "../PNG/PNGWriter.h"
 
 using namespace ZNAC;
 using namespace Visualize;
@@ -9,14 +11,29 @@ using namespace Visualize;
 int main()
 {
 	Window wnd("test", true);
-	PFNGLDEBUGMESSAGECALLBACKPROC v = (PFNGLDEBUGMESSAGECALLBACKPROC)glfwGetProcAddress("glDebugMessageCallback");
-	std::cout << ((v == nullptr)?"zannnenndesita":"omedetou") << std::endl;
-	v(0, 0);
-	glClearColor(1, 1, 1, 0);
+	glClearColor(1, 1, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3d(0, 0, 0);
+	glBegin(GL_LINE_STRIP);
+	glVertex2d(1, 0);
+	glVertex2d(-1, 0);
+	glVertex2d(0, 1);
+	glVertex2d(0, -1);
+	glEnd();
+
+	glfwSwapBuffers(wnd);
+
+	Capture c(wnd);
+
+	c.Get();
+
+	PNGWriter w("./", "test", c.Width, c.Height);
+
+	w << c.Image;
+
 	while(wnd)
 	{
 		wnd.Update();
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(wnd);
 	}
 }
